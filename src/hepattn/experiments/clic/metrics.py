548c34b3ca9298b4176f9d66fc632d269a01d1c7
sunnyw22 <sunnywong200111@gmail.com>
2025-07-17 10:45:32 +0100

    Linting

--- src/hepattn/experiments/clic/metrics.py
+++ src/hepattn/experiments/clic/metrics.py
@@ -2,6 +2,7 @@ import torch
 
 
 class MaskInference:
+    @staticmethod
     def basic_sigmoid(pred):
         """
         Assign hits to tracks if they have a high matching probability.
@@ -10,6 +11,7 @@ class MaskInference:
         pred = pred.sigmoid() > 0.5
         return pred
 
+    @staticmethod
     def basic_argmax(pred):
         """
         Assign hits to the track with the highest probability.
@@ -20,6 +22,7 @@ class MaskInference:
         pred[idx, torch.arange(len(idx))] = True
         return pred
 
+    @staticmethod
     def weighted_argmax(pred, class_preds):
         """
         Assign hits to the track with the highest probabilithy, weighted with class pred confidence.
@@ -31,16 +34,19 @@ class MaskInference:
         pred[idx, torch.arange(len(idx))] = True
         return pred
 
+    @staticmethod
     def exact_match(pred, tgt):
         """Perfect hit to track assignment"""
         if len(tgt) == 0:
             return torch.tensor(torch.nan)
         return (pred == tgt).all(-1).float().mean()
 
+    @staticmethod
     def eff(pred, tgt):
         """Efficiency to assign correct hit to track"""
         return ((pred & tgt).sum(-1) / tgt.sum(-1)).mean()
 
+    @staticmethod
     def pur(pred, tgt):
         """Purity of assigned hits on tracks"""
         return ((pred & tgt).sum(-1) / pred.sum(-1)).mean()
