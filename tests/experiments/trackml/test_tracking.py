bde55bf72f96ad6926888a0a704a75f8541ef865
svanstro <sam.van.stroud@cern.ch>
2025-08-14 09:26:45 +0100

    cleanup tests

--- tests/experiments/trackml/test_tracking.py
+++ tests/experiments/trackml/test_tracking.py
@@ -2,15 +2,5 @@ from hepattn.experiments.trackml import run_tracking
 
 
 def test_run_tracking():
-    args = [
-        "fit",
-        "--config",
-        "tests/experiments/trackml/test_tracking.yaml",
-        "--trainer.fast_dev_run",
-        "1",
-        "--data.dummy_data",
-        "True",
-        "--trainer.logger",
-        "False",
-    ]
+    args = ["fit", "--config", "tests/experiments/trackml/test_tracking.yaml"]
     run_tracking.main(args)
