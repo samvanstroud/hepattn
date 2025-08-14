af1b4b7739f6a236be9624038687f4ce6acf8c80
Sam VS <sam.van.stroud@cern.ch>
2025-08-14 10:19:41 +0100

    Update tests/experiments/clic/test_clic.py
    Co-authored-by: Dmitrii Kobylianskii <kobad2@gmail.com>

--- tests/experiments/clic/test_clic.py
+++ tests/experiments/clic/test_clic.py
@@ -1,6 +1,6 @@
 from hepattn.experiments.clic import main
 
 
-def test_run_tracking():
+def test_run_pflow():
     args = ["fit", "--config", "tests/experiments/clic/test_clic.yaml"]
     main.main(args)
