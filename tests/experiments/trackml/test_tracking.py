7873b3300cc69855269a449dc4d0b17e14d5fcde
Sam <sam.van.stroud@cern.ch>
2025-09-08 16:04:21 +0100

    fix tests

--- tests/experiments/trackml/test_tracking.py
+++ tests/experiments/trackml/test_tracking.py
@@ -3,9 +3,9 @@ from hepattn.experiments.trackml import run_tracking
 from ..utils import run_test  # noqa: TID252
 
 
-def run_test_tracking() -> None:
+def test_tracking() -> None:
     run_test(run_tracking, "tests/experiments/trackml/test_tracking.yaml")
 
 
-def run_test_tracking_old_sort() -> None:
+def test_tracking_old_sort() -> None:
     run_test(run_tracking, "tests/experiments/trackml/test_tracking_old_sort.yaml")
