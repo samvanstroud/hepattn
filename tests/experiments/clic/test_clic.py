5c2c13a6d3dfe360f34a0c4c450149a2357ecd1e
Sam <sam.van.stroud@cern.ch>
2025-09-08 15:51:38 +0100

    update

--- tests/experiments/clic/test_clic.py
+++ tests/experiments/clic/test_clic.py
@@ -2,5 +2,5 @@ from hepattn.experiments.clic import main
 from tests.experiments.test_utils import test_run
 
 
-def test_run_pflow():
+def test_run_pflow() -> None:
     test_run(main, "tests/experiments/clic/test_clic.yaml", "clic_test_")
--- tests/experiments/trackml/test_tracking.py
+++ tests/experiments/trackml/test_tracking.py
@@ -3,9 +3,9 @@ from hepattn.experiments.trackml import run_tracking
 from ..test_utils import test_run  # noqa: TID252
 
 
-def test_run_tracking():
+def test_run_tracking() -> None:
     test_run(run_tracking, "tests/experiments/trackml/test_tracking.yaml", "tracking-test_", "test_logs")
 
 
-def test_run_tracking_old_sort():
+def test_run_tracking_old_sort() -> None:
     test_run(run_tracking, "tests/experiments/trackml/test_tracking_old_sort.yaml", "tracking-test-old-sort_", "test_logs")
