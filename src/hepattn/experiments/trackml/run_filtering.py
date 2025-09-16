bb69c2cd977c2dc60b672f60e14647b137c3b735
Philippa <philippa.duckett@outlook.com>
2025-09-16 10:31:14 +0100

    bug fix

--- src/hepattn/experiments/trackml/run_filtering.py
+++ src/hepattn/experiments/trackml/run_filtering.py
@@ -17,8 +17,13 @@ class TrackMLFilter(ModelWrapper):
         super().__init__(name, model, lrs_config, optimizer)
 
     def log_custom_metrics(self, preds, targets, stage):
-        pred = preds["final"]["hit_filter"]["hit_on_valid_particle"]
-        true = targets["hit_on_valid_particle"]
+
+        task = self.model.tasks[0]
+        target_field = getattr(task, "target_field", "on_valid_particle")
+        input_object = getattr(task, "input_object", "hit")  # e.g., "key"
+        expected_key = f"{input_object}_{target_field}"
+        pred = preds["final"]["hit_filter"][expected_key]
+        true = targets[expected_key]
 
         tp = (pred * true).sum()
         tn = ((~pred) * (~true)).sum()
