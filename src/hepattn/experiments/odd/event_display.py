7dcc2ed32161035c2ca3a380bb3199fbd78916d4
Max Hart <maxhart@compute-gpu-0-3.local>
2025-11-14 02:34:58 +0000

    update odd event display

--- src/hepattn/experiments/odd/event_display.py
+++ src/hepattn/experiments/odd/event_display.py
@@ -6,13 +6,9 @@ plt.rcParams["figure.dpi"] = 300
 
 
 def plot_odd_event(data, axes_spec, object_name, batch_idx=0, valid=True, mark_transparent=None, label_objects=False, gridspec_kw=None):
-    # Setup the axes
-    num_axes = len(axes_spec)
-
-    fig, ax = plt.subplots(1, num_axes, gridspec_kw=gridspec_kw)
-    fig.set_size_inches(8 * num_axes, 8)
-
-    ax = [ax] if num_axes == 1 else ax.flatten()
+    fig, ax = plt.subplots(1, len(axes_spec), gridspec_kw=gridspec_kw)
+    fig.set_size_inches(8 * len(axes_spec), 8)
+    ax = [ax] if len(axes_spec) == 1 else ax.flatten()
 
     # Setup the color cycler that will be used
     colormap = plt.cm.tab20
@@ -28,7 +24,6 @@ def plot_odd_event(data, axes_spec, object_name, batch_idx=0, valid=True, mark_t
             y = data[f"{input_name}_{ax_spec['y']}"][batch_idx]
 
             ax[ax_idx].scatter(x, y, alpha=0.25, s=1.0, color="black")
-
             num_object_slots = data[f"{object_name}_{input_name}_valid"][batch_idx].shape[-2]
 
             for object_idx in range(num_object_slots):
@@ -49,18 +44,14 @@ def plot_odd_event(data, axes_spec, object_name, batch_idx=0, valid=True, mark_t
                     if input_name in sihit_names:
                         # Used for sorting the hits in time when we want to plot them in order in the tracker
                         idx = torch.argsort(data[f"{input_name}_time"][batch_idx][mask], dim=-1)
-
                         ax[ax_idx].plot(x[mask][idx], y[mask][idx], color=color, marker="o", alpha=alpha, linewidth=1.0, ms=2.0, linestyle=linestyle)
 
                     # Calo hits
                     elif input_name in calohit_names:
                         energy = torch.sqrt(1e6 * data[f"{input_name}_total_energy"][batch_idx])
                         ax[ax_idx].scatter(x[mask], y[mask], color=color, marker=".", alpha=0.6, s=energy[mask])
-                
-
 
             ax[ax_idx].set_xlabel(ax_spec.get("xlabel", ax_spec["x"]))
             ax[ax_idx].set_ylabel(ax_spec.get("ylabel", ax_spec["y"]))
-            # ax[ax_idx].set_aspect("equal", "box")
 
     return fig
--- src/hepattn/experiments/odd/scripts/plot_event.py
+++ src/hepattn/experiments/odd/scripts/plot_event.py
@@ -9,6 +9,7 @@ from hepattn.experiments.odd.data import ODDEventDataModule, ODDEventDataset
 from hepattn.experiments.odd.event_display import plot_odd_event
 
 
+# Setup the dataloader
 config_path = Path("src/hepattn/experiments/odd/configs/base.yaml")
 config = yaml.safe_load(config_path.read_text())["data"]
 config["num_workers"] = 0
@@ -18,27 +19,51 @@ datamodule.setup(stage="test")
 test_dataloader = datamodule.test_dataloader()
 data_iter = iter(test_dataloader)
 
+# Read from the dataset
+for _i in range(2):
+    inputs, targets = next(data_iter)
 
-inputs, targets = next(data_iter)
-
-inputs, targets = next(data_iter)
-
-for k, v in inputs.items():
-    print("input:  ", k, v.shape)
-
-for k, v in targets.items():
-    print("target: ", k, v.shape)
-
-ax_spec = [
-    {"x": "x", "y": "y", "input_names": ["sihit", "calohit"], "xlabel": r"Global $x$", "ylabel": r"Global $y$"},
-    {"x": "z", "y": "y", "input_names": ["sihit", "calohit"], "xlabel": r"Global $z$", "ylabel": r"Global $y$"}
-]
 data = inputs | targets
 
+for k, v in data.items():
+    print(k, v.shape)
+
+# Define plotting config
+plot_save_dir = Path("src/hepattn/experiments/odd/plots/event_displays/")
+plot_save_dir.mkdir(exist_ok=True, parents=True)
+
+# Plot full detector for particles
 fig = plot_odd_event(
     data,
-    ax_spec,
+    [
+        {"x": "x", "y": "y", "input_names": ["sihit", "calohit"], "xlabel": r"Global $x$", "ylabel": r"Global $y$"},
+        {"x": "z", "y": "y", "input_names": ["sihit", "calohit"], "xlabel": r"Global $z$", "ylabel": r"Global $y$"}
+    ],
     "particle",
 )
 
-fig.savefig("/share/rcifdata/maxhart/hepattn/src/hepattn/experiments/odd/display.png")
+fig.savefig(plot_save_dir / Path("particles_event_display.png"))
+
+# Plot tracker for particles
+fig = plot_odd_event(
+    data,
+    [
+        {"x": "x", "y": "y", "input_names": ["sihit"], "xlabel": r"Global $x$", "ylabel": r"Global $y$"},
+        {"x": "z", "y": "y", "input_names": ["sihit"], "xlabel": r"Global $z$", "ylabel": r"Global $y$"}
+    ],
+    "particle",
+)
+
+fig.savefig(plot_save_dir / Path("particles_event_display_tracker.png"))
+
+# Plot tracker for tracks
+fig = plot_odd_event(
+    data,
+    [
+        {"x": "x", "y": "y", "input_names": ["sihit"], "xlabel": r"Global $x$", "ylabel": r"Global $y$"},
+        {"x": "z", "y": "y", "input_names": ["sihit"], "xlabel": r"Global $z$", "ylabel": r"Global $y$"}
+    ],
+    "track",
+)
+
+fig.savefig(plot_save_dir / Path("tracks_event_display_tracker.png"))
