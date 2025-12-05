ed589e14b4ce2cd37d8bb2069a04511997db2a9a
pduckett <philippa.duckett@outlook.com>
2025-12-05 11:16:04 +0000

    fix duplicates in attn mask logging

--- src/hepattn/callbacks/attn_mask_logger.py
+++ src/hepattn/callbacks/attn_mask_logger.py
@@ -262,4 +262,5 @@ class AttnMaskLogger(Callback):
         # only process if this batch is selected by the sampler
         if batch_idx % self.log_every_n_batches != 0:
             return
-        self._process_attention_masks_from_outputs(pl_module, outputs, batch_idx, is_validation=False)
+        step = getattr(trainer, "global_step", batch_idx)
+        self._process_attention_masks_from_outputs(pl_module, outputs, step, is_validation=False)
